81c15a0314884e52ed5d098157864fe6f83a7fb6
Renee Nordholm <reneenordholm@gmail.com>
2019-05-15 17:13:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,11 @@ class Song
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    song
+    return song
   end
-
+  
+  def self.create_by_name(name)
+    song = self.new
+    
+  end
 end
