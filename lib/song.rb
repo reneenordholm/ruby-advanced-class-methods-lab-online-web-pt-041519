e116ffa695cdd603f3c767a623964548edcec05d
Renee Nordholm <reneenordholm@gmail.com>
2019-05-15 17:22:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,5 +41,10 @@ class Song
     @@all.sort_by { |song| song.name }
   end
   
-  def self.new_from_filename
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
 end
