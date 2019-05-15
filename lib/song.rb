df394c2b43893badf969ebc27edf2d8805876d28
Renee Nordholm <reneenordholm@gmail.com>
2019-05-15 17:20:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
     self.find_by_name(name) || self.create_by_name(name)
   end
   
-  def self.alphabetical(name)
+  def self.alphabetical()
     @@all.sort_by { |song| song.name }
   end
 end
